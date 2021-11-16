218e7d47c4eb07e589482d36ab514816189a3cad
Sarah Rosanna Busch <annarose0113@gmail.com>
2021-11-15 18:55:35 -0800

    refactored to reduce iframes, add shop

--- main.js
+++ main.js
@@ -18,31 +18,40 @@ var main = (function() {
     that.init = function() {
         elem.menu = f.html.getElem('#menu');
         elem.sidePanel = f.html.getElem('#sidePanel');
+        elem.shop = f.html.getElem("#shop");
         footer.setup();
-        if(document.documentElement.clientWidth >= 800) {
-            main.nav("./pages/myChess/index.html");
-        }
+        // if(document.documentElement.clientWidth >= 800) {
+        //     main.nav("./pages/shop/shop.html");
+        // }
     }
 
-    that.nav = function(url, callback) {
+    that.nav = function(url) {
         url = !url ? openPage : url;
-        f.http.get(url, function(pageData) {
-            elem.sidePanel.innerHTML = pageData;
-            if(document.documentElement.clientWidth < 800) {
-                elem.menu.style.display = 'none';
-            } else {
-                elem.menu.style.display = 'flex';
-            }
+        if(url === 'shop') {
+            elem.shop.style.display = 'block';
+            f.html.empty(elem.sidePanel);
+            elem.sidePanel.display = 'none';
+        } else {
+            elem.shop.style.display = 'none';
+            elem.sidePanel.display = 'inline-block';
+            f.http.get(url, function(pageData) {
+                elem.sidePanel.innerHTML = pageData;
+                if(document.documentElement.clientWidth < 800) {
+                    elem.menu.style.display = 'none';
+                } else {
+                    elem.menu.style.display = 'flex';
+                }
 
-            switch(url) {
-                case "./pages/illustrations/vectorArt.html":
-                    illustrations.load("vectorArt");
-                    break;
-                case "./pages/illustrations/techDrawings.html":
-                    illustrations.load("techDrawings");
-                    break;
-            }
-        });
+                switch(url) {
+                    case "./pages/illustrations/vectorArt.html":
+                        illustrations.load("vectorArt");
+                        break;
+                    case "./pages/illustrations/techDrawings.html":
+                        illustrations.load("techDrawings");
+                        break;
+                }
+            });
+        }
     }
 
     return that;
