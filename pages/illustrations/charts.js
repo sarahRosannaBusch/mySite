816465e9f5837f042ff9fc52238517a9d1e4b488
Sarah Rosanna Busch <annarose0113@gmail.com>
2022-12-07 11:51:43 -0800

    sorting by column

--- pages/illustrations/charts.js
+++ pages/illustrations/charts.js
@@ -4,6 +4,7 @@ var charts = (function() {
     var countries = {};
     var salaries = {};
     var tableData = [];
+    var sortMask = [1,1,1,1];
 
     that.init = function() {
         elem.salaryTable = f.html.getElem("#webDevSalaries");
@@ -28,31 +29,65 @@ var charts = (function() {
                     var countryCode = salaryData._links.self.href.slice(-12, -10);
                     if(webDevData?.job?.id === "WEB-DEVELOPER") {
                         salaries[countryCode] = webDevData.salary_percentiles;
-                        //console.log(webDevData.salary_percentiles);
+                        //console.log(webDevData.salary_percentiles);                        
+                        var sal = salaries[countryCode];
+                        var rowData = [
+                            countries[countryCode], 
+                            Math.round(sal.percentile_25), 
+                            Math.round(sal.percentile_50), 
+                            Math.round(sal.percentile_75)
+                        ];
+                        tableData.push(rowData);
+                        _addRows(rowData);
                     } else {
-                        salaries[countryCode] = {percentile_25: 0, percentile_50: 0, percentile_75: 0};
+                        //salaries[countryCode] = {percentile_25: 0, percentile_50: 0, percentile_75: 0};
                         //console.log("no salary data available for: " + countryCode);
-                    }
-                    var sal = salaries[countryCode];
-                    var rowData = [
-                        countries[countryCode], 
-                        Math.round(sal.percentile_25), 
-                        Math.round(sal.percentile_50), 
-                        Math.round(sal.percentile_75)
-                    ];
-                    tableData.push(rowData);
-                    var row = elem.salaryTable.insertRow(-1); //-1 to add at end
-                    for(var d = 0; d < 4; d++) {
-                        var cell = row.insertCell(d);
-                        cell.innerHTML = rowData[d];
                     }
                     if(rxCount >= count) {
                         //we have all the data now
-                        console.log(JSON.stringify(tableData));
+                        //console.log(JSON.stringify(tableData));
+                        var sortIds = ["country", "low", "med", "high"];
+                        var numCols = sortIds.length;
+                        for(var c = 0; c < numCols; c++) {
+                            var buttonId = sortIds[c];
+                            elem["sort_" + buttonId] = f.html.getElem("#" + buttonId);
+                            elem["sort_" + buttonId].onclick = function() {
+                                console.log("sort by " + this.id);
+                                f.html.empty(elem.salaryTable);
+                                var col = sortIds.indexOf(this.id);
+                                var ascending = (sortMask[col] > 0) ? true : false;
+                                tableData.sort(function(a, b) {
+                                    //sort function derived from https://stackoverflow.com/questions/16096872/how-to-sort-2-dimensional-array-by-column-value
+                                    if (a[col] === b[col]) {
+                                        return 0;
+                                    } else {
+                                        if(ascending) {
+                                            return (a[col] < b[col]) ? -1 : 1;
+                                        } else {               
+                                            return (a[col] > b[col]) ? -1 : 1;  
+                                        }
+                                    }
+                                });
+                                sortMask[col] *= -1;
+                                //console.log(JSON.stringify(tableData));
+                                var len = tableData.length;
+                                for(var n = 0; n < len; n++) {
+                                    var rowData = tableData[n];
+                                    _addRows(rowData);
+                                }
+                            }
+                        }
                     }
                 });
             }
         });
+        function _addRows(rowData) {
+            var row = elem.salaryTable.insertRow(-1); //-1 to add at end
+            for(var d = 0; d < 4; d++) {
+                var cell = row.insertCell(d);
+                cell.innerHTML = rowData[d];
+            }
+        }
     }
 
     return that;
